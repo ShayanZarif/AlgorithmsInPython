6152bc18c13a4cc499d5ea89aef3be96ad092044
ShayanZarif <37469632+ShayanZarif@users.noreply.github.com>
2018-10-26 21:39:39 +0600

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,3 +1,13 @@
+"""
+The below code is used only to describe how this algorithm works.
+The below exact procedure can be done just by using .sort() function.e.g.myList.sort().
+To use the above method to make it sort the list in descending order,
+we have to do   myList.sort(reverse=True).
+
+
+
+This method is only available in Python and JavaScript.(There may be some other languages too.)
+"""
 def bubblesort(myList):
 	for i in range(0,len(myList)-1):
 		for j in range(0 , len(myList)-1 - i):
@@ -8,3 +18,9 @@ def bubblesort(myList):
 
 alist = ['a' , 's' , 't' , 'b' , 'l' , 'i']
 print(bubblesort(alist))
+"""
+To make the actual code sort in reverse procedure we have to change the 
+'if myList[j] > myList[j+1]'
+to 
+'if myList[j] < myList[j+1]
+"""
