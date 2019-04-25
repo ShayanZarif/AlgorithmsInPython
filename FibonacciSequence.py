1816a7c6bc437ee1cde4044a08f60c40b728d5da
ShayanZarif <37469632+ShayanZarif@users.noreply.github.com>
2019-04-25 12:35:48 +0600

    Add files via upload

---
+++ FibonacciSequence.py
@@ -0,0 +1,18 @@
+# Python program to display the Fibonacci sequence up to n-th term using recursive functions
+
+def recur_fibo(n):
+   """Recursive function to
+   print Fibonacci sequence"""
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 10
+
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
