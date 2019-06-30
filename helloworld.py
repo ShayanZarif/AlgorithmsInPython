5c26cee07d4ccf6e64365744b14362a3c33c614e
ShayanZarif <37469632+ShayanZarif@users.noreply.github.com>
2019-06-30 13:24:07 +0600

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+from sklearn import tree
+
+features = [[150,0] , [170,0] , [140,1] , [130,1]]# 0 is bumpy and 1 is smooth
+labels = [0,0,1,1]#0 is apple and 
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features , labels)
+print clf.predict([[150 , 0]])
