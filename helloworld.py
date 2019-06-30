8cb244867b8b9e3b1b7b12f36a40dcbc7df0e785
ShayanZarif <37469632+ShayanZarif@users.noreply.github.com>
2019-06-30 13:24:51 +0600

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 from sklearn import tree
 
 features = [[150,0] , [170,0] , [140,1] , [130,1]]# 0 is bumpy and 1 is smooth
-labels = [0,0,1,1]#0 is apple and 
+labels = [0,0,1,1]#0 is apple and 1 is orange
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features , labels)
 print clf.predict([[150 , 0]])
